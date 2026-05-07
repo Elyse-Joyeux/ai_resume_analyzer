5ce24cd8eedfda6f0c2be8dca9ec5e9882c6ced8
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-05-07 16:38:43 +0200

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,8 +1,10 @@
 import type { Route } from "./+types/home";
-import Navbar from '../components/Navbar'
-import {resumes} from '../../constants/index'
-import ResumeCard from '../components/ResumeCard'
-
+import Navbar from "../components/Navbar";
+import { resumes } from "../../constants/index";
+import ResumeCard from "../components/ResumeCard";
+import {usePuterStore} from '~/lib/puter'
+import {useLocation, useNavigate} from 'react-router'
+import {useEffect} from 'react'
 
 
 export function meta({}: Route.MetaArgs) {
@@ -13,23 +15,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className = "bg-[url('/assets/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
 
-    <section className="main-section">
-      <div className="page-headinng py-16">
-        <h1>Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedback</h2>
-      </div>
-    
-    {resumes.length > 0 && (
-      <div className="resumes-section">
-        {resumes.map((resume)=>(
-        <ResumeCard key={resume.id} resume={resume}/>
-    ))}
-      </div>
-    )}
-    </section>
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated]);
+
+  return (
+    <main className="bg-[url('/assets/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+      <section className="main-section">
+        <div className="page-headinng py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
     </main>
+  );
 }
