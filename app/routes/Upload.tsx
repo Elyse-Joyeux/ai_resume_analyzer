4e0ab353ef58bb73f92be5c36a049dc0063001fa
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-05-07 21:47:59 +0200

    resume upload

--- app/routes/Upload.tsx
+++ app/routes/Upload.tsx
@@ -1,30 +1,80 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import {useState} from 'react'
+import Navbar from "../components/Navbar";
+import FileUploader from "../components/FileUploader"
+import React from "react";
+import { type FormEvent, useState } from "react";
 
 const Upload = () => {
-    const [isProcessing, setIsProcessing] = useState(true)
-    const [statusText, setStatusText] = useState('')
+  const [isProcessing, setIsProcessing] = useState(true);
+  const [statusText, setStatusText] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
-     <main className="bg-[url('/assets/images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/assets/images/bg-main.svg')] bg-cover">
       <Navbar />
 
       <section className="main-section">
-        <div className="page-heading">
-            <h1>Smart feeback for your dream job</h1>
-            {isProcessing ? (
-                <>
-                    <h2>{statusText}</h2>
-                    <img src="/assets/images/resume-scan.gif" alt="resume scanning" className="w-full" />
-                </>
-            ) : (
-                <h2>Drop your resume for an ATS score and improvement tips</h2>
-            )}
+        <div className="page-heading py-16">
+          <h1>Smart feeback for your dream job</h1>
+          {isProcessing ? (
+            <>
+              <h2>{statusText}</h2>
+              <img
+                src="/assets/images/resume-scan.gif"
+                alt="resume scanning"
+                className="w-full"
+              />
+            </>
+          ) : (
+            <h2>Drop your resume for an ATS score and improvement tips</h2>
+          )}
+          {isProcessing && (
+            <form
+              id="upload-form"
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-4 mt-8"
+            >
+              <div className="form-div">
+                <label htmlFor="company-name">Company Name</label>
+                <input
+                  type="text"
+                  name="company-name"
+                  placeholder="Company Name"
+                  id="company-name"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-title">Job Title</label>
+                <input
+                  type="text"
+                  name="job-title"
+                  placeholder="Job Title"
+                  id="job-title"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="job-description">Job Description</label>
+                <textarea
+                  rows={5}
+                  name="job-description"
+                  placeholder="Job Description"
+                  id="job-description"
+                />
+              </div>
+              <div className="form-div">
+                <label htmlFor="uploader">Upload Resume</label>
+                <FileUploader />
+              </div>
+              <button className="primary-button" type="submit">
+                Analyze Resume
+              </button>
+            </form>
+          )}
         </div>
       </section>
-      </main>
-  )
-}
+    </main>
+  );
+};
 
-export default Upload
+export default Upload;
