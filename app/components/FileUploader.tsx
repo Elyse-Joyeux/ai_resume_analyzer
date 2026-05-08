08a7ec731dd3cc00d2b7317c0b2e72e32042acac
Elyse-Joyeux <elysejoyeux590@gmail.com>
2026-05-08 10:27:08 +0200

    upload stage fixed

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -5,43 +5,94 @@ interface FileUploaderProps {
   onFileSelect?: (file: File | null) => void;
 }
 
+const formatSize = (bytes: number) => {
+  if (bytes < 1024 * 1024) {
+    return `${(bytes / 1024).toFixed(1)} KB`;
+  }
+
+  if (bytes < 1024 * 1024 * 1024) {
+    return `${(bytes / (1024 * 1024)).toFixed(1)} MB`;
+  }
+
+  return `${(bytes / (1024 * 1024 * 1024)).toFixed(1)} GB`;
+};
+
 const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState<File | null>(null);
 
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
       // Do something with the files
       const file = acceptedFiles[0] || null;
+      setFile(file);
       onFileSelect?.(file);
     },
     [onFileSelect],
   );
-  const { getRootProps, getInputProps, acceptedFiles, isDragActive } =
-    useDropzone({
-      onDrop,
-      multiple: false,
-      accept: { "application/pdf": [".pdf"] },
-      maxSize: 20 * 1024 * 1024,
-    });
 
-    const file = acceptedFiles[0] || null
+  const { getRootProps, getInputProps, inputRef } = useDropzone({
+    onDrop,
+    multiple: false,
+    accept: { "application/pdf": [".pdf"] },
+    maxSize: 20 * 1024 * 1024,
+  });
+
+  const removeFile = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+      event.stopPropagation();
+      setFile(null);
+      onFileSelect?.(null);
+
+      if (inputRef.current) {
+        inputRef.current.value = "";
+      }
+    },
+    [inputRef, onFileSelect],
+  );
 
   return (
     <div className="w-full gradient-border">
       <div {...getRootProps()}>
         <input {...getInputProps()} />
         <div className="space-y-4 cursor-pointer">
-          <div className="mx-auto w-16 h-16 flex items-center justify-center">
-            <img
-              src="/assets/icons/info.svg"
-              alt="upload"
-              className="size-20"
-            />
-          </div>
           {file ? (
-            <div></div>
+            <div
+              className="uploader-selected-file"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <img src="/assets/images/pdf.png" alt="pdf" className="size-10" />
+              <div className="flex items-center space-x-3">
+                <div>
+                  <p className="text-sm text-gray-700 font-medium truncate max-w-xs">
+                    {file.name}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
+              <button
+                type="button"
+                className="p-2 cursor-pointer"
+                onClick={removeFile}
+              >
+                <img
+                  src="/assets/icons/cross.svg"
+                  alt="remove"
+                  className="w-4 h-4"
+                />
+              </button>
+            </div>
           ) : (
             <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                <img
+                  src="/assets/icons/info.svg"
+                  alt="upload"
+                  className="size-20"
+                />
+              </div>
               <p className="text-lg text-gray-500">
                 <span className="font-semibold">Click to upload</span> or drap
                 or drop
